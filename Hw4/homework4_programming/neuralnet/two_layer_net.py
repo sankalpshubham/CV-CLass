9b2d65df61540cb9753eb837f0c5d2b2e8d277b0
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2023-10-24 18:05:05 -0500

    udpates

---
+++ Hw4/homework4_programming/neuralnet/two_layer_net.py
@@ -0,0 +1,106 @@
+import numpy as np
+from classifier import Classifier
+from layers import fc_forward, fc_backward, relu_forward, relu_backward
+
+
+class TwoLayerNet(Classifier):
+    """
+    A neural network with two layers, using a ReLU nonlinearity on its one
+    hidden layer. That is, the architecture should be:
+
+    input -> FC layer -> ReLU layer -> FC layer -> scores
+    """
+    def __init__(self, input_dim=3072, num_classes=10, hidden_dim=512,
+                 weight_scale=1e-3):
+        """
+        Initialize a new two layer network.
+
+        Inputs:
+        - input_dim: The number of dimensions in the input.
+        - num_classes: The number of classes over which to classify
+        - hidden_dim: The size of the hidden layer
+        - weight_scale: The weight matrices of the model will be initialized
+          from a Gaussian distribution with standard deviation equal to
+          weight_scale. The bias vectors of the model will always be
+          initialized to zero.
+        """
+        #######################################################################
+        # TODO: Initialize the weights and biases of a two-layer network.     #
+        #######################################################################
+
+        self.W1 = weight_scale * np.random.randn(input_dim, hidden_dim)
+        self.b1 = np.zeros(hidden_dim)
+        self.W2 = weight_scale * np.random.randn(hidden_dim, num_classes)
+        self.b2 = np.zeros(num_classes)
+        
+        #######################################################################
+        #                          END OF YOUR CODE                           #
+        #######################################################################
+
+    def parameters(self):
+        params = None
+        #######################################################################
+        # TODO: Build a dict of all learnable parameters of this model.       #
+        #######################################################################
+
+        params = {
+            'W1': self.W1,
+            'b1': self.b1,
+            'W2': self.W2,
+            'b2': self.b1
+        }
+
+        #######################################################################
+        #                          END OF YOUR CODE                           #
+        #######################################################################
+        return params
+
+    def forward(self, X):
+        scores, cache = None, None
+        #######################################################################
+        # TODO: Implement the forward pass to compute classification scores   #
+        # for the input data X. Store into cache any data that will be needed #
+        # during the backward pass.                                           #
+        #######################################################################
+
+        hidden_layer, hidden_cache = fc_forward(X, self.W1, self.b1)
+        hidden_layer_relu, relu_cache = relu_forward(hidden_layer)
+
+        scores, scores_cache = fc_forward(hidden_layer_relu, self.W2, self.b2)
+        cache = (hidden_cache, relu_cache, scores_cache)
+
+        #######################################################################
+        #                          END OF YOUR CODE                           #
+        #######################################################################
+        return scores, cache
+
+    def backward(self, grad_scores, cache):
+        grads = None
+        #######################################################################
+        # TODO: Implement the backward pass to compute gradients for all      #
+        # learnable parameters of the model, storing them in the grads dict   #
+        # above. The grads dict should give gradients for all parameters in   #
+        # the dict returned by model.parameters().                            #
+        #######################################################################
+
+        hidden_cache, relu_cache, scores_cache = cache
+    
+        d_hidden_layer_relu, dW2, db2 = fc_backward(grad_scores, scores_cache)
+        d_hidden_layer = relu_backward(d_hidden_layer_relu, relu_cache)
+        dX, dW1, db1 = fc_backward(d_hidden_layer, hidden_cache)
+
+        # Accumulate gradients for biases (b2 should be updated with db2)
+        db2 = np.sum(db2, axis=0)
+        db1 = np.sum(db1, axis=0)
+
+        grads = {
+            'W1': dW1,
+            'b1': db1,
+            'W2': dW2,
+            'b2': db2
+        }
+
+        #######################################################################
+        #                          END OF YOUR CODE                           #
+        #######################################################################
+        return grads
